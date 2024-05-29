26f05657cfd2b6ff26ff3cbaedfa4343c2ebbce1
307489 <142132396+307489@users.noreply.github.com>
2024-05-30 00:24:04 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
-function addToCart(bookTitle) {
-    const cartItems = document.getElementById('cart-items');
-    const cartItem = document.createElement('li');
-    cartItem.className = 'cart-item';
-    cartItem.textContent = bookTitle;
-    cartItems.appendChild(cartItem);
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+
+    searchForm.classList.remove('active');
+
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+window.onload = () => {
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
 }
